18f933c70e570977155ac748be6540ee49671438
wangyibo5843 <wangyibo5843@163.com>
2016-11-24 09:41:11 +0800

    add react demo

--- reactjs/hello-world2.js
+++ reactjs/hello-world2.js
@@ -2,6 +2,6 @@
  * Created by Administrator on 2016/11/22.
  */
 ReactDOM.render(
-<h1>Hello, world!</h1>,
-    document.getElementById('example')
+<h1>Hello, world!</h1>,  /**只能有一个一级html标签,如果写成这样就报错<h1></h1><h3></h3>*/
+    document.getElementById('example')  /***最后一行不能加标点，否则报错**/
 );
